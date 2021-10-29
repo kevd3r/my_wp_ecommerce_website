cbd6eb3efc872422800d6f7e39b09fb10a02f3ae
kevd3r <64579607+kevd3r@users.noreply.github.com>
2021-10-29 09:14:42 +0200

    resolving form problems

--- app/public/wp-content/themes/storefront-child/assets/custom-js/dist/rhinos_registration_modal.dev.js
+++ app/public/wp-content/themes/storefront-child/assets/custom-js/dist/rhinos_registration_modal.dev.js
@@ -1,19 +1,41 @@
 "use strict";
 
 jQuery(document).ready(function ($) {
+  /**----------  Variables for modal's gestion */
   var modal = document.getElementById('modal-content');
+  /**get the modal header's datas */
+
   var btn = document.getElementById('user_connect');
   var close = document.getElementById('close');
+  /** modal's first display to log in with email & login */
+
   var modalLogin = document.getElementById('modal-body');
   var logoutButton = document.getElementById('logout-button');
+  /**modal's logout screen display */
+
   var modalLogout = document.getElementById('modal-logout');
   var newUser = document.getElementById('new-user');
+  /** modal's new user screen display*/
+
   var modalNewUser = document.getElementById('modal-new-user');
+  var userLogin = document.getElementById('user_login');
+  /** get the 1st & 2nd passwords' id's because of a conflict
+   * with same id's
+   */
+
+  var newUserLogin = document.querySelectorAll('#modal-body>#loginform>.login-password>input');
+  var tmp = [];
+  var newId;
+  newUserLogin.forEach(function (e) {
+    tmp.push(e.getAttribute('id'));
+    console.log(e);
+    newId = e.getAttribute('id');
+  });
+  console.log(newId);
   btn.addEventListener('click', function () {
     modal.style.display = "block";
     modalLogout.style.display = "none";
     modalNewUser.style.display = "none";
-    console.log('match');
     $('.modal-header h2').text("Log In");
   });
   close.addEventListener('click', function () {
@@ -24,10 +46,8 @@ jQuery(document).ready(function ($) {
   logoutButton.addEventListener('click', function () {
     modalLogin.style.display = "none";
     modalLogout.style.display = "block";
-    console.log('yeaaaaaaah !');
   });
   newUser.addEventListener('click', function () {
-    console.log('WTF ?!!!');
     modalLogin.style.display = "none";
     modalLogout.style.display = "none";
     modalNewUser.style.display = "block";
--- app/public/wp-content/themes/storefront-child/header.php
+++ app/public/wp-content/themes/storefront-child/header.php
@@ -54,8 +54,7 @@
 			<a href=""><button id="user_connect"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   			<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
 				</svg></button>
-			</a>
-			
+			</a>	
 		</div>
 <!-- Modal content -->
 		<div class="modal-content" id="modal-content" role="dialog" tabindex="-1">
@@ -71,7 +70,7 @@
 				<?php echo"vous n'êtes plus connecté."?>
 			</div>
 			<div class="modal-body" id="modal-new-user">
-				<?php echo do_shortcode( '[user_registration_form id="402"]' );?>
+				<?php /*echo do_shortcode( '[user_registration_form id="402"]' );*/?>
 			</div>
 		</div>
 	</header><!-- #masthead -->
